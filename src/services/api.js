fa5d9256806e202d8169ad06769cc74a311d1b20
İrem Çelik <iremcelik6@stu.aydin.edu.tr>
2025-06-09 08:03:03 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://moviehub-9b64.onrender.com/api';
+const API_BASE_URL = 'https://moviehub-9b64.onrender.com:8080/api';
 
 // API instance oluştur
 const api = axios.create({
